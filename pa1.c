f53f862adcdb82598e43ae3e1b68073c6490e01c
granthoechst <granthoechst@college.harvard.edu>
2016-02-16 23:28:34 -0500

    Working along

--- pa1.c
+++ pa1.c
@@ -9,15 +9,22 @@ float** case_1_randgen (int seed, int v)
 	else
 		srand(seed);
 
-	float** array = malloc(v * size_of(float));
+	float** array = malloc(v * sizeof(float*));
 
 	for (int i = 1 ; i < v ; i++)
 	{
-		array[i] = malloc(i * size_of(float));
+		array[i] = malloc(i * sizeof(float));
 		for (int j = 0 ; j < i ; j++)
 			array[i][j] = (float) rand() / (float) RAND_MAX ;
 	}
 
 	return array;
 
+}
+
+int main(int argc, char *argv[]) {
+	if (argc == 3)
+		float** arr = case_1_randgen(atoi(argv[1]), atoi(argv[2]));
+	else
+		printf("Yo! You suck!\n");
 }
